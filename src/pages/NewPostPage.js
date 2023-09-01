5311a6b2950033d2725a711135840399b5e10770
John <wheeler6123@gmail.com>
2023-09-01 09:33:11 -0600

    added handleSubmit function to NewPostPage

---
+++ src/pages/NewPostPage.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Container } from 'react-bootstrap';
+
+
+const NewPostPage = () => {
+
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const post = {
+            title,
+            content
+        };
+        await axios.post('http://localhost:3000/posts', post);
+        navigate('/');
+    }
+
+
+}
