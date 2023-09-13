576d0d085d9ae34a61641f9f8cddb5d35cbb61db
John <wheeler6123@gmail.com>
2023-09-13 14:56:23 -0600

    built out page content for HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Card, Button, Container, Row, Column } from 'react-bootstrap';
+import { Card, Button, Container, Row, Column, Col } from 'react-bootstrap';
 
 const HomePage = () => {
 
@@ -20,6 +20,25 @@ const HomePage = () => {
         const newPosts = posts.filter(post => post.id !== id);
         setPosts(newPosts);
     }
+return (
+    <Container>
+        <Row>
+            {posts.map(post => (
+                <Col md={4} key={post.id} className='mb-4'>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant='top' src={post.image} alt={post.title}/>
+                        <Card.Body>
+                            <Card.Title>{post.title}</Card.Title>
+                            <Card.Subtitle className='mb-2 text-muted'>By: {post.author}</Card.Subtitle>
+                            <Card.Text>{post.content}</Card.Text>
+                            <Button variant='primary' className='mr-2' as={Link} to={`/posts/${post.id}`}>Read More</Button>
+                            <Button variant='danger' onClick={() => handleDelete(post.id)}>Delete</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    </Container>
+)
 
-    
 }
