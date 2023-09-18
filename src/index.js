1e8ee550e879cbff58553c0cec863144df9fe63f
John <wheeler6123@gmail.com>
2023-09-18 15:29:35 -0600

    removed strict mode from index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(<App />);
